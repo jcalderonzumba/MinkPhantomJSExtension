4f987aac232adbe7d77e660e02ae9cc0fdc73da0
Christophe Coevoet <stof@notk.org>
2015-09-22 13:42:46 +0200

    Remove useless class existence check
    The extension has a dependency on the driver in the composer metadata,
    so the driver class will always be installed.

--- src/ServiceContainer/Driver/PhantomJSFactory.php
+++ src/ServiceContainer/Driver/PhantomJSFactory.php
@@ -51,11 +51,6 @@ class PhantomJSFactory implements DriverFactory {
    * @return Definition
    */
   public function buildDriver(array $config) {
-    if (!class_exists('Zumba\Mink\Driver\PhantomJSDriver')) {
-      throw new \RuntimeException(
-        'Install PhantomJSDriver in order to use phantomjs driver.'
-      );
-    }
     return new Definition('Zumba\Mink\Driver\PhantomJSDriver',
       array($config["phantom_server"], $config["template_cache"])
     );
